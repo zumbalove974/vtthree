4639d9f94502ae88b52fbd3bf1f28278765c15d7
María Jesus Lobo <mjesuslobo@gmail.com>
2021-01-04 15:15:04 +0100

    adding tiff reading and example function to add a cube

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,19 @@
 import "regenerator-runtime/runtime";
+import * as THREE from "three";
 import Feature from "ol/Feature";
 import { VTController } from "./VTController";
 import { mergedRender, singleRender } from "./VTThreeViewer";
 import { planStyle, grisStyle, muetStyle } from "./OLViewer";
 import proj4 from "proj4";
 import { proj4326, proj3857 } from "./Utils";
+
+//data can be imported like this or read from the data folder
 import windData from "../../data/wind.json";
 import covidData from "../../data/covid_data.json";
+import * as geotiff from "geotiff";
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width = window.innerWidth; // this makes the 3D canvas full screen
+const height = window.innerHeight; // this makes the 3D canvas full screen
 
 let parisLatLon = [48.8534, 2.3488];
 let parisCenter = proj4(proj4326, proj3857, [parisLatLon[1], parisLatLon[0]]);
@@ -32,9 +36,12 @@ const paramsWind = {
 };
 
 let params = paramsWind;
-
+let controller = null;
 async function init() {
-  let controller = new VTController(
+  // to read tiff file: https://geotiffjs.github.io/geotiff.js/. other files to be read should be added to the data folder
+  // let tiffData = await geotiff.fromUrl("Hauteurs.tif");
+
+  controller = new VTController(
     width,
     height,
     params.center, //center coordinates in webmercator
@@ -44,6 +51,19 @@ async function init() {
     params.style, //style for the tiles
     false
   );
+  addObjects();
+}
+
+function addObjects() {
+  //example to add an object to the scene
+  let worldCoords = controller.threeViewer.getWorldCoords(vavinCenter); // the getWorldCoords function transform webmercator coordinates into three js world coordinates
+  var geometry = new THREE.BoxBufferGeometry(100, 100, 100);
+  var material = new THREE.MeshStandardMaterial({ color: 0xff4500 });
+  var cube = new THREE.Mesh(geometry, material); //a three js mesh needs a geometry and a material
+  cube.position.x = worldCoords[0];
+  cube.position.y = worldCoords[1];
+  cube.position.z = 0;
+  controller.threeViewer.scene.add(cube); //all objects have to be added to the threejs scene
 }
 
 init();
